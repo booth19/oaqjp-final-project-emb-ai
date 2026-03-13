305abd2aaf97d0dc85086b9922047fc75b151f10
booth19 <brendandbooth@gmail.com>
2026-03-13 11:14:28 -0400

    add new files

---
+++ emotion_detection.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze) : 
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+# requires headers
+    headers = {
+    "Content-Type" : "application/json",
+    "grpc-metadata-mm-model-id" : "emotion_aggregated-workflow_lang_en_stock"
+    }
+# Input JSON payload
+    payload = {
+        "raw_document" : {
+            "text" : text_to_analyze
+        }
+    }
+
+# Send POST request
+    response = requests.post(url, headers=headers, json=payload)
+
+#convert response to JSON
+    result = response.json()
+
+# extract emotion scores
+    emotions = result["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+
+
+# determine required dominant emotion
+    dominant = max(emotions, key=emotions.get)
+
+    return {
+        "anger" : anger_score,
+        "disgust" : disgust_score,
+        "fear" : fear_score,
+        "joy" : joy_score,
+        "sadness" : sadness_score,
+        "dominant_emotion" : dominant
+    }
